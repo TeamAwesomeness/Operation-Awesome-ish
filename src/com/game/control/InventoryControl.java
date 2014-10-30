83bc9598a9d1f9a3f5100facb3a9f29764efb794
Sterling <sterling@silveroasis.us>
2014-10-29 21:12:40 -0700

    Added InventoryControl Added com.game.control

---
+++ src/com/game/control/InventoryControl.java
@@ -0,0 +1,55 @@
+package com.game.control;
+
+import com.game.src.Bread;
+import com.game.src.Chickens;
+import com.game.src.Potions;
+import com.game.src.Lives;
+/**
+ *
+ * @author Sterling Grant && Moses Accinelli
+ */
+// Inventory Control
+/*
+ * calcMaxWeight(inventoryItem, volume) : double
+ * saveBackpackSize(inventoryItem, backpack) : double
+ * calcBackpacksNeeded(inventoryItem, backpack, dailyAmountRequired, NoPeople) : long
+ * calcBreadNeeded(inventoryItem, noBackpacks) : long
+ * calcBreadNeeded(Health) : double
+ * calcChickensNeeded(backpack, noBackpacks) : long
+ * calcChickensNeeded(Health) : double
+ * calcPotionsNeeded(backpack, noBackpacks) : long
+ * calcPotionsNeeded(Health) : double
+ * calcLivesNeeded(backpack, noBackpacks) : long
+ * calcLivesNeeded(Health) : double
+ * getAmountToGeneralStore(inventoryItem) : double
+ * getInventoryList() : inventoryItems
+ * addToInventory(inventoryItem, amount)
+ * removeFromInventory(inventoryItem, amount)
+*/
+public class InventoryControl {
+    Bread breadFood = new Bread();
+    Chickens chickensFood = new Chickens();
+    Potions potionsFood = new Potions();
+    Lives lives = new Lives();
+    
+    public double calcMaxWeight(double inventoryItem, double volume) {
+        //Maximum Weight
+        if(inventoryItem == 4 && volume >= 9) { //Bread, Chickens, Potions
+            System.out.println("\nYou are at a maximum inventory.");
+        } else {
+            System.out.println("\n\nBread: " + breadFood +
+                               "\nChickens: " + chickensFood + 
+                               "\nPotions: " + potionsFood);
+        }
+    
+        //Minimum Weight 
+        if(inventoryItem == 0) {
+            System.out.println("\nYou are at a minimum inventory.");
+        } else {
+            System.out.println("\n\nBread: " + breadFood +
+                               "\nChickens: " + chickensFood + 
+                               "\nPotions: " + potionsFood);
+        }
+        return 0;
+    }
+}
